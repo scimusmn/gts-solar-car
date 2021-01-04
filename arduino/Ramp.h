4d4c8c36a759d6bfb294c6021e603444f05e6300
eojreyem <jmeyer@smm.org>
2021-01-04 13:55:06 -0600

    a little more

--- arduino/Ramp.h
+++ arduino/Ramp.h
@@ -1,39 +1,40 @@
-// Provides linear ramping (fade on, fade off) control of a PWM pin 
+// Provides linear ramping (fade on, fade off) control of a PWM pin
 
 #include "Arduino.h"
 
 #ifndef Ramp_h
 #define Ramp_h
 
-class Ramp {
+class Ramp
+{
 public:
-  Ramp(bool, int, long);
+  Ramp(bool, int, long); // isActiveHigh, pin, acceleration (millis to go 0 to 100%)
 
-  int targetDutyCycle;  // percentage, a value from 0 to 100.    
+  int targetDutyCycle; // percentage, a value from 0 to 100.
   int currentDutyCycle;
   bool isActiveHigh;
-  long millisFullSwing; // time in milliseconds to go from 0% to 100% or back.  
-  
-  void Ramp(bool _isActiveHigh = true, int _pin, long _millisFullSwing) {
-    pwmPin = _pin
+
+  void Ramp(bool _isActiveHigh = true, int _pin, long _millis0to100)
+  {
+    pwmPin = _pin;
+    millis0to100 = _millis0to100;
     isActiveHigh = _isActiveHigh;
-    pinMode(output, pwmPin);
+    pinMode(pwmPin, OUTPUT);
     if (_start_low)
-      analogWrite(0);
+      analogWrite(pwmPin, 0);
     else
-      analogWrite(255);
+      analogWrite(pwmPin, 255);
   }
 
   void update()
+  {
+  }
 
 private:
   int pwmPin;
-  long changePerMillis
-  int currentDutyCycle;
+  long millis0to100; // time in milliseconds to go from 0% to 100% or back.
+  long changePerMillis int currentDutyCycle;
   unsigned long currentMillis, millisLastChange;
-
-  
-
 };
 
 #endif
